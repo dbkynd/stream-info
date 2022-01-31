2fbad184d5db505b4072ab0fea133f9d7bd88d31
DBKynd <db@dbkynd.com>
2022-01-30 18:10:35 -0700

    test console.log ci lint

---
+++ backend/.eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  plugins: ['import', '@typescript-eslint'],
+  extends: [
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist'],
+  rules: {
+    'no-console': process.env.NODE_ENV !== 'production' ? 'warn' : 'error',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { argsIgnorePattern: 'next' },
+    ],
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+    'import/first': 'error',
+    'import/no-mutable-exports': 'error',
+    'import/newline-after-import': 'error',
+    'sort-imports': ['error', { ignoreDeclarationSort: true }],
+  },
+};
---
+++ backend/src/app.ts
@@ -0,0 +1,10 @@
+// import * as server from './server'
+
+export async function start(): Promise<void> {
+  console.log('APP START');
+  // server.connect()
+}
+
+export async function stop(): Promise<void> {
+  // await server.disconnect()
+}
