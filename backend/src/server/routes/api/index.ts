ec0c04cffb925855972c27a0ae3947c314c42387
DBKynd <db@dbkynd.com>
2022-02-04 22:41:04 -0700

    add lists api endpoint

--- backend/src/server/routes/api/index.ts
+++ backend/src/server/routes/api/index.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import CheerService from '../../../database/lib/cheer';
+import HostService from '../../../database/lib/host';
 
 const router = express.Router();
 
@@ -8,6 +10,18 @@ router.get('/', (req, res, next) => {
 
 router.get('/user', (req, res, next) => {
   res.status(200).json(req.user);
+});
+
+router.get('/lists', async (req, res, next) => {
+  try {
+    const payload = {
+      cheers: await CheerService.list(),
+      hosts: await HostService.list(),
+    };
+    res.status(200).json(payload);
+  } catch (e) {
+    next(e);
+  }
 });
 
 export default router;
