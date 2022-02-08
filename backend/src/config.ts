5e592103258c6b95710c1a09f52f0cdb2d7617fe
DBKynd <db@dbkynd.com>
2022-02-07 22:33:41 -0700

    dayjs

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,6 +1,6 @@
 export const timeTemplate = "Anne's clock reads: <TIME>.";
 export const timeTz = 'America/Los_Angeles'; // https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
-export const timeFormat = 'ccc, h:mma ZZZZ'; // https://moment.github.io/luxon/#/formatting
+export const timeFormat = 'ddd, h:mma z'; // https://day.js.org/docs/en/display/format
 
 export const uptimeOffline = 'The stream is currently offline.';
 export const uptimeTemplate = 'The stream has been live for <UPTIME>.';
--- backend/src/server/services/time/time_service.ts
+++ backend/src/server/services/time/time_service.ts
@@ -1,7 +1,14 @@
-import { DateTime } from 'luxon';
+import dayjs from 'dayjs';
+import advancedFormat from 'dayjs/plugin/advancedFormat';
+import timezone from 'dayjs/plugin/timezone';
+import utc from 'dayjs/plugin/utc';
 import { timeFormat, timeTemplate, timeTz } from '../../../config';
 
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.extend(advancedFormat);
+
 export default (): string => {
-  const time = DateTime.now().setZone(timeTz).toFormat(timeFormat);
+  const time = dayjs().tz(timeTz).format(timeFormat);
   return timeTemplate.replace('<TIME>', time);
 };
