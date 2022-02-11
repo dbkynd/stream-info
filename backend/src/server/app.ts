260970af2ea22159000793abc74139749ae6abd5
DBKynd <dave@dbkynd.com>
2022-02-11 12:57:58 -0700

    temp remove csp

--- backend/src/server/app.ts
+++ backend/src/server/app.ts
@@ -1,12 +1,5 @@
 import path from 'path';
 import express from 'express';
-import {
-  expressCspHeader,
-  NONCE,
-  SELF,
-  INLINE,
-  EVAL,
-} from 'express-csp-header';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import * as logger from '../logger';
@@ -18,19 +11,10 @@ import sessionStore from './sessionStore';
 
 const app = express();
 
-// app.use(helmet());
+app.use(helmet());
 const format = process.env.NODE_ENV === 'production' ? 'combined' : 'dev';
 app.use(morgan(format, { stream: logger.stream }));
 app.use(express.json());
-
-app.use(
-  expressCspHeader({
-    directives: {
-      'script-src': [SELF],
-    },
-    reportOnly: true,
-  }),
-);
 
 app.use(sessionStore);
 app.use(passport.initialize());
