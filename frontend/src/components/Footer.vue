dd9567fe84c2f5d521d705f8eade9ed2243e116c
DBKynd <dave@dbkynd.com>
2022-02-14 13:43:11 -0700

    vuetify footer

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,23 +1,49 @@
 <template>
-  <footer>
-    SOME FOOTER DATA
-  </footer>
+  <v-footer fixed padless bottom width="100%" :class="{expanded: expanded}">
+    <v-row>
+      <v-col cols="5" class="left">
+        LEFT
+      </v-col>
+      <v-col cols="2" class="middle">
+        <v-btn @click="expanded = !expanded">
+          <v-icon v-if="expanded">mdi-chevron-double-down</v-icon>
+          <v-icon v-else>mdi-chevron-double-up</v-icon>
+        </v-btn>
+      </v-col>
+      <v-col cols="5" class="right">
+        RIGHT
+      </v-col>
+    </v-row>
+  </v-footer>
 </template>
 
 <script>
 export default {
-  name: "Footer"
+  name: "Footer",
+  data() {
+    return {
+      height: '3rem',
+      expanded: false,
+    }
+  },
 }
 </script>
 
 <style scoped>
-footer {
-  width: 100%;
-  position: absolute;
-  bottom: 0;
-  height: 2rem;
-  z-index: 2;
-  opacity: 1;
-  background-color: red;
+v-footer {
+  height: 1rem;
 }
+.expanded {
+  height: 30rem;
+}
+.left {
+  text-align: left;
+}
+.middle {
+  text-align: center;
+}
+.right {
+  text-align: right;
+}
+
 </style>
