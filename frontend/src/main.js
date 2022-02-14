18cc33ea49a2f64c295d277ba2d823606665fd0e
DBKynd <dave@dbkynd.com>
2022-02-14 13:46:07 -0700

    remove webfontloader

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify'
-import { loadFonts } from './plugins/webfontloader'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import vuetify from './plugins/vuetify';
 
-loadFonts()
-
-createApp(App)
-  .use(router)
-  .use(store)
-  .use(vuetify)
-  .mount('#app')
+createApp(App).use(router).use(store).use(vuetify).mount('#app');
