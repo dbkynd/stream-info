77e9e04860759f176005128b315b2274fd4a3586
DBKynd <dave@dbkynd.com>
2022-02-14 11:59:50 -0700

    add stubs

---
+++ stubs/tip.ts
@@ -0,0 +1,14 @@
+export default {
+  _id: "6207e4c0939ebba3b878f702",
+  payload: {
+    tipId: "6207e494454c194194c6110f",
+    username: "DBKynd",
+    amount: 5,
+    currency: "USD",
+    message: "Tip Message",
+    avatar: "https://cdn.streamelements.com/static/default-avatar.png",
+  },
+  cleared: true,
+  createdAt: "2022-02-12T16:47:16.605Z",
+  __v: 0,
+};
