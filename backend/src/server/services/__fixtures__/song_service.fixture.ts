82ca7a1e43076832506421257c6f770136b1969b
DBKynd <db@dbkynd.com>
2022-02-16 19:53:34 -0700

    song service tests

---
+++ backend/src/server/services/__fixtures__/song_service.fixture.ts
@@ -0,0 +1,146 @@
+export const noSongPlaying: LastFmResponse = {
+  recenttracks: {
+    '@attr': {
+      page: '1',
+      total: '40469',
+      user: 'AnneMunition',
+      perPage: '1',
+      totalPages: '40469',
+    },
+    track: [
+      {
+        artist: {
+          mbid: '66fc5bf8-daa4-4241-b378-9bc9077939d2',
+          '#text': 'Tool',
+        },
+        album: {
+          mbid: '3c583861-4e54-4b9c-a518-ad313b0383f5',
+          '#text': 'Lateralus',
+        },
+        image: [
+          {
+            size: 'small',
+            '#text':
+              'https://lastfm.freetls.fastly.net/i/u/34s/b5a5721a08264207c2df36bf07454005.jpg',
+          },
+          {
+            size: 'medium',
+            '#text':
+              'https://lastfm.freetls.fastly.net/i/u/64s/b5a5721a08264207c2df36bf07454005.jpg',
+          },
+          {
+            size: 'large',
+            '#text':
+              'https://lastfm.freetls.fastly.net/i/u/174s/b5a5721a08264207c2df36bf07454005.jpg',
+          },
+          {
+            size: 'extralarge',
+            '#text':
+              'https://lastfm.freetls.fastly.net/i/u/300x300/b5a5721a08264207c2df36bf07454005.jpg',
+          },
+        ],
+        streamable: '0',
+        date: {
+          uts: '1615483488',
+          '#text': '11 Mar 2021, 17:24',
+        },
+        url: 'https://www.last.fm/music/Tool/_/Schism',
+        name: 'Schism',
+        mbid: '004a4829-ac45-3ada-9df3-e94dd7c7a32b',
+      },
+    ],
+  },
+};
+
+export const songPlaying: LastFmResponse = {
+  recenttracks: {
+    '@attr': {
+      page: '1',
+      total: '23971',
+      user: 'DBKynd',
+      perPage: '1',
+      totalPages: '23971',
+    },
+    track: [
+      {
+        artist: {
+          mbid: 'b95ce3ff-3d05-4e87-9e01-c97b66af13d4',
+          '#text': 'Eminem',
+        },
+        '@attr': {
+          nowplaying: 'true',
+        },
+        mbid: '011bad40-6540-4157-9ef6-c3f79698d3b8',
+        album: {
+          mbid: '156a7344-89e1-49fb-b3d3-9525c1ee5a5f',
+          '#text': 'The Marshall Mathers LP',
+        },
+        streamable: '0',
+        url: 'https://www.last.fm/music/Eminem/_/Stan',
+        name: 'Stan',
+        image: [
+          {
+            size: 'small',
+            '#text':
+              'https://lastfm.freetls.fastly.net/i/u/34s/de87486b44ed660d8bc9b5fa9a416336.png',
+          },
+          {
+            size: 'medium',
+            '#text':
+              'https://lastfm.freetls.fastly.net/i/u/64s/de87486b44ed660d8bc9b5fa9a416336.png',
+          },
+          {
+            size: 'large',
+            '#text':
+              'https://lastfm.freetls.fastly.net/i/u/174s/de87486b44ed660d8bc9b5fa9a416336.png',
+          },
+          {
+            size: 'extralarge',
+            '#text':
+              'https://lastfm.freetls.fastly.net/i/u/300x300/de87486b44ed660d8bc9b5fa9a416336.png',
+          },
+        ],
+      },
+      {
+        artist: {
+          mbid: 'd1fc999f-6184-41a6-bcb1-7c59bf74a6e1',
+          '#text': 'K.Flay',
+        },
+        album: {
+          mbid: '22d51ddd-6000-4889-9e5d-eb3ae85d560c',
+          '#text': 'Life as a Dog',
+        },
+        image: [
+          {
+            size: 'small',
+            '#text':
+              'https://lastfm.freetls.fastly.net/i/u/34s/fd6e5f6eb5b34263c2e0fd465e227e7f.png',
+          },
+          {
+            size: 'medium',
+            '#text':
+              'https://lastfm.freetls.fastly.net/i/u/64s/fd6e5f6eb5b34263c2e0fd465e227e7f.png',
+          },
+          {
+            size: 'large',
+            '#text':
+              'https://lastfm.freetls.fastly.net/i/u/174s/fd6e5f6eb5b34263c2e0fd465e227e7f.png',
+          },
+          {
+            size: 'extralarge',
+            '#text':
+              'https://lastfm.freetls.fastly.net/i/u/300x300/fd6e5f6eb5b34263c2e0fd465e227e7f.png',
+          },
+        ],
+        streamable: '0',
+        date: {
+          uts: '1615513662',
+          '#text': '12 Mar 2021, 01:47',
+        },
+        url: 'https://www.last.fm/music/K.Flay/_/Wishing+It+Was+You',
+        name: 'Wishing It Was You',
+        mbid: '3a65abb8-8dd8-432b-9a51-0031e5f03065',
+      },
+    ],
+  },
+};
---
+++ backend/src/server/services/__tests__/song_service.spec.ts
@@ -0,0 +1,91 @@
+import nock from 'nock';
+import { songNotPlaying } from '../../../config';
+import * as songServiceFixtures from '../__fixtures__/song_service.fixture';
+import SongService from '../song/song_service';
+
+describe('song service module', () => {
+  beforeEach(() => {
+    process.env.LASTFM_API_KEY = 'foobar';
+  });
+
+  test('no LASTFM_API_KEY set', async () => {
+    delete process.env.LASTFM_API_KEY;
+    const song = await SongService('someUser');
+    expect(song).toBe('LastFM API Key not set.');
+  });
+
+  test('LASTFM 500 status', async () => {
+    nock('https://ws.audioscrobbler.com').get('/2.0').query(true).reply(500);
+
+    const song = await SongService('someUser');
+    expect(song).toBe('LastFM Error');
+  });
+
+  test('no tracks found', async () => {
+    nock('https://ws.audioscrobbler.com')
+      .get('/2.0')
+      .query({
+        method: 'user.getrecenttracks',
+        limit: 1,
+        user: 'someUser',
+        api_key: process.env.LASTFM_API_KEY,
+        format: 'json',
+      })
+      .reply(200, {});
+
+    const song = await SongService('someUser');
+    expect(song).toBe('No recent tracks found.');
+  });
+
+  test('no now playing track found', async () => {
+    nock('https://ws.audioscrobbler.com')
+      .get('/2.0')
+      .query({
+        method: 'user.getrecenttracks',
+        limit: 1,
+        user: 'someUser',
+        api_key: process.env.LASTFM_API_KEY,
+        format: 'json',
+      })
+      .reply(200, songServiceFixtures.noSongPlaying);
+
+    const song = await SongService('someUser');
+    expect(song).toBe(songNotPlaying);
+  });
+
+  test('now playing track found', async () => {
+    nock('https://ws.audioscrobbler.com')
+      .get('/2.0')
+      .query({
+        method: 'user.getrecenttracks',
+        limit: 1,
+        user: 'someUser',
+        api_key: process.env.LASTFM_API_KEY,
+        format: 'json',
+      })
+      .reply(200, songServiceFixtures.songPlaying);
+
+    const song = await SongService('someUser');
+    expect(song).toBe('Stan by Eminem');
+  });
+
+  test('now playing track found but missing track and artist', async () => {
+    const songData = Object.assign({}, songServiceFixtures.songPlaying);
+    songData.recenttracks.track[0].name = '';
+    songData.recenttracks.track[0].artist['#text'] = '';
+
+    nock('https://ws.audioscrobbler.com')
+      .get('/2.0')
+      .query({
+        method: 'user.getrecenttracks',
+        limit: 1,
+        user: 'someUser',
+        api_key: process.env.LASTFM_API_KEY,
+        format: 'json',
+      })
+      .reply(200, songData);
+
+    const song = await SongService('someUser');
+    expect(song).toBe('Track by Artist');
+  });
+});
