463dd0ef4fa77032dba2e903eb152eae8b61c7d6
DBKynd <dave@dbkynd.com>
2022-03-22 16:18:26 -0600

    update logout page

--- frontend/src/views/Logout.vue
+++ frontend/src/views/Logout.vue
@@ -1,15 +1,36 @@
 <template>
-<div>
-  YAU HAVE BEEN LOGGED OUT
-</div>
+  <div class="notfound">
+    <h1>You have successfully logged out.</h1>
+    <div>
+      <v-img src="@/assets/peepoLeave.gif" alt="" />
+    </div>
+    <div>
+      <v-btn color="purple" @click="login">Login</v-btn>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "Logout"
+  name: "Logout",
+  methods: {
+    login() {
+      window.location.href = '/api/auth/login';
+    },
+  }
 }
 </script>
 
 <style scoped>
+.notfound {
+  font-size: 2rem;
+  color: #fff;
+  text-align: center;
+  margin-top: 5rem;
+}
 
+.v-img {
+  margin-top: 1em;
+  height: 250px;
+}
 </style>
