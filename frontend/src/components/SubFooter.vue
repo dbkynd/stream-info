74c39c8c0cd427a120f108a3d4836ebf4e138a8e
DBKynd <db@dbkynd.com>
2022-05-16 22:00:38 -0600

    sub footer icons

--- frontend/src/components/SubFooter.vue
+++ frontend/src/components/SubFooter.vue
@@ -3,14 +3,17 @@
     <div class="icons">
       <div v-if="subPlan">
         <div v-if="subPlan === 'Prime'">
-          <img :src="require('@/assets/prime.png')" alt="" />
+          <img :src="require('@/assets/prime.png')" alt="Prime" />
         </div>
         <div v-else-if="subPlan === '2000'">
-          <img :src="require('@/assets/silver_star.png')" alt="" />
+          <img :src="require('@/assets/silver_star.png')" alt="Tier2" />
         </div>
         <div v-else-if="subPlan === '3000'">
-          <img :src="require('@/assets/gold_star.png')" alt="" />
+          <img :src="require('@/assets/gold_star.png')" alt="Tier3" />
         </div>
+      </div>
+      <div v-if="userstate['msg-id'].includes('gift')">
+        <img :src="require('@/assets/gift-icon.png')" alt="Gift" />
       </div>
     </div>
   </div>
