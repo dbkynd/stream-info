8fee5db0dca690ca0253ec0de87332d5ef2521e1
DBKynd <db@dbkynd.com>
2022-09-03 21:10:22 -0600

    remove custom toast style import

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,8 +6,6 @@ import vuetify from './plugins/vuetify';
 import Toast, { POSITION } from 'vue-toastification';
 import 'vue-toastification/dist/index.css';
 
-import '@/styles/toasts.scss';
-
 const toastDefaultOptions = {
   pauseOnFocusLoss: false,
   position: POSITION.TOP_RIGHT,
