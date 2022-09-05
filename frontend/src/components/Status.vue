afbcb166d69fb49df57355d39320cc8822db5c6a
DBKynd <db@dbkynd.com>
2022-09-04 21:41:23 -0600

    add roomstate status
    used format from chatty window title bar

--- frontend/src/components/Status.vue
+++ frontend/src/components/Status.vue
@@ -1,26 +1,62 @@
 <template>
+  <v-row no-gutters align="center">
     <div class="status_indicators">
       <div :class="{ enabled: status.clientWs }" title="Backend WebSocket"></div>
       <div :class="{ enabled: status.twitchIrc }" title="Twitch Chat"></div>
       <div :class="{ enabled: status.seWs }" title="StreamElements WebSocket"></div>
     </div>
+    <div class="roomstate">
+      {{ states }}
+    </div>
+  </v-row>
 </template>
 
 <script>
+import {mapState} from 'vuex'
+
 export default {
   name: "Status",
   computed: {
+    ...mapState(['roomstate', 'appState']),
     status() {
       return this.$store.state.appState;
+    },
+    slow() {
+      const slow = this.roomstate['slow']
+      return slow === false ? null : `Slow: ${slow}`
+    },
+    followers() {
+      const followers = this.roomstate['followers-only']
+      return followers === "-1" ? null : `Followers: ${followers === false ? 0 : followers}m`;
+    },
+    sub() {
+      const sub = this.roomstate['subs-only']
+      return sub === false ? null : 'Sub'
+    },
+    r9k() {
+      const r9k = this.roomstate['r9k']
+      return r9k === false ? null : 'r9k'
+    },
+    emote() {
+      const emote = this.roomstate['emote-only']
+      return emote === false ? null : 'EmoteOnly'
+    },
+    raidmode() {
+      const raidmode = this.appState.raidmode
+      return raidmode === false ? null : 'Raidmode'
+    },
+    states() {
+      const arr = [this.slow, this.sub, this.followers, this.r9k, this.emote, this.raidmode]
+      return `[${arr.filter(x => x).join('|')}]`;
     }
-  }
+  },
 }
 </script>
 
 <style scoped>
 .status_indicators {
-  display:flex;
-  align-items:center;
+  display: flex;
+  align-items: center;
   padding: 4px 0 4px;
   margin-left: 8px;
 }
@@ -35,5 +71,10 @@ export default {
 
 .status_indicators > div.enabled {
   background-color: #397441;
+}
+
+.roomstate {
+  font-size: 14px;
+  color: #bababa;
 }
 </style>
