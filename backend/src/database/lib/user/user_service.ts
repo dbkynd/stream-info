d790bf05358bf68d61329908a29f478ee11ccd1c
DBKynd <db@dbkynd.com>
2022-09-22 02:54:01 -0600

    use mongoose schema defaults

--- backend/src/database/lib/user/user_service.ts
+++ backend/src/database/lib/user/user_service.ts
@@ -1,18 +1,7 @@
-import defaults from '../../../server/defaultUserSettings';
 import User, { UserDoc } from './user_model';
 
 async function updateProfile(profile: any): Promise<UserDoc> {
-  const user = await User.findOneAndUpdate(
-    { twitchId: profile.id },
-    { profile },
-    { upsert: true, new: true },
-  );
-  if (!user.settings || !Object.keys(user.settings).length) {
-    await updateSettings(profile.id, defaults);
-    const newUser = await getUser(profile.id);
-    if (newUser) return newUser;
-  }
-  return user;
+  return User.findOneAndUpdate({ twitchId: profile.id }, { profile }, { upsert: true, new: true });
 }
 
 async function updateSettings(id: string, settings: any): Promise<void> {
