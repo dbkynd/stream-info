e6f4ee5fed3bbad60bf96237655c6d9c3b00489b
DBKynd <db@dbkynd.com>
2022-10-02 17:14:38 -0600

    add more stats

--- backend/src/server/services/stats/stats_service.ts
+++ backend/src/server/services/stats/stats_service.ts
@@ -1,8 +1,11 @@
+import { getAppState, getRoomstate } from '../../../events/state';
 import * as token from '../../../token';
 
 export default () => {
   return {
-    validToken: false, //token.isValid(),
+    validToken: token.isValid(),
     requiredScopes: token.getScopes(),
+    appState: getAppState(),
+    roomState: getRoomstate(),
   };
 };
